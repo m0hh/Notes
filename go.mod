6f75bdc52eea6e730de5e54db7e63ae9419e7d1a
m0hh <lordegy53@gmail.com>
2025-04-27 18:55:54 +0300

    Fisrt Commit

---
+++ go.mod
@@ -0,0 +1,21 @@
+module github.com/m0hh/Notes
+
+go 1.23.0
+
+toolchain go1.23.8
+
+require (
+	github.com/felixge/httpsnoop v1.0.4
+	github.com/go-mail/mail/v2 v2.3.0
+	github.com/julienschmidt/httprouter v1.3.0
+	github.com/lib/pq v1.10.9
+	github.com/m0hh/smart-logitics v0.0.0-20240421121010-f444f314f3f1
+	github.com/tomasen/realip v0.0.0-20180522021738-f0c99a92ddce
+	golang.org/x/crypto v0.37.0
+	golang.org/x/time v0.11.0
+)
+
+require (
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+	gopkg.in/mail.v2 v2.3.1 // indirect
+)
---
+++ internal/data/models.go
@@ -0,0 +1,27 @@
+package data
+
+import (
+	"database/sql"
+	"errors"
+)
+
+var (
+	ErrRcordNotFound    = errors.New("record not found")
+	ErrEditConflict     = errors.New("edit conflict")
+	ErrUniqueFood       = errors.New("unique food with serving failed")
+	ErrWrongForeignKey  = errors.New("wrong Foreign key")
+	ErrWrongCredentials = errors.New("wrong credentials")
+	ErrFKConflict       = errors.New("Foriegn Key conflicr")
+)
+
+type Models struct {
+	Tokens TokenModel
+	Users  UserModel
+}
+
+func NewModels(db *sql.DB) Models {
+	return Models{
+		Tokens: TokenModel{DB: db},
+		Users:  UserModel{DB: db},
+	}
+}
